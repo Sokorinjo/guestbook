d17de56d426eab24a46bfceafc4157234c9b2474
Sokorinjo <vsokorac65@gmail.com>
2025-06-20 19:09:16 +0200

    Added react router.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './App.tsx'
+import Welcome from './pages/WelcomePage.tsx'
+import NewMessagePage from './pages/NewMessagePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} >
+          <Route index={true} element={<Welcome />} />
+          <Route path='add-message' element={<NewMessagePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
