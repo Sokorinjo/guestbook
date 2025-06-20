62a4b95e2c980b75e9564af395731f169cca097f
Sokorinjo <vsokorac65@gmail.com>
2025-06-20 19:10:48 +0200

    Working on NewMessagePage

---
+++ frontend/src/pages/NewMessagePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const NewMessagePage = (props: Props) => {
+    return (
+        <>
+            <header>
+                <h1> Add a message</h1>
+            </header>
+        </>
+    )
+}
+
+export default NewMessagePage
