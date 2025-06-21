b06fe90b5a491ca86b2eefcf512959a160e51c59
Sokorinjo <vsokorac65@gmail.com>
2025-06-21 16:27:19 +0200

    Added React query provider.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Outlet />
-    </div>
+    </QueryClientProvider>
   )
 }
 
