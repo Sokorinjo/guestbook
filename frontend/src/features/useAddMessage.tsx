61f4127d1f1e89cff32c511cf4db360f566e5daf
Sokorinjo <vsokorac65@gmail.com>
2025-06-23 23:34:49 +0200

    Add message mutation.

---
+++ frontend/src/features/useAddMessage.tsx
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+type Message = {
+    message: string
+    name: string
+}
+
+const addMessage = async (newMessage: Message) => {
+    const { data } = await axios.post('api/messages', newMessage)
+    return data
+}
+
+export const useAddMessage = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: addMessage,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['messages'] })
+        },
+    })
+}
