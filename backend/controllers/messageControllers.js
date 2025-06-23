3de17ad3b37c7b3c8b879455fe9cc9acda2218ff
Sokorinjo <vsokorac65@gmail.com>
2025-06-23 18:47:21 +0200

    Added controllers for messages.

---
+++ backend/controllers/messageControllers.js
@@ -0,0 +1,34 @@
+import { pool as db } from "../config/db.js"
+
+//@desc Add a message
+//@route POST /api/messages/
+const addMessage = (req, res) => {
+    const {message, name} = req.body
+    try{
+        const sql = `INSERT INTO messages(message, name) values(?, ?)`
+        const result = db.query(sql,[message, name])
+        res.send(result)
+    }
+    catch(err){
+        throw new Error(err)
+    }
+}
+
+//@desc Get all messages
+//@route GET /api/messages
+const getAllMessages = async (req, res) => {
+    try{
+        const sql = `SELECT * FROM messages;`
+        const result = await db.query(sql)
+        console.log(result)
+        res.send(result)
+
+    }catch(err){
+        throw new Error(err)
+    }
+}
+
+export const messageControllers = {
+    addMessage,
+    getAllMessages
+}
