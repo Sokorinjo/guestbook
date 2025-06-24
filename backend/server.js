57c6f91ff2034dce0d4f0801fbca2f904224de2c
Sokorinjo <vsokorac65@gmail.com>
2025-06-24 17:17:19 +0200

    CORS

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { router as messageRouter } from "./routes/messageRoutes.js";
 import {connectDB} from "./config/db.js";
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config()
@@ -14,6 +15,12 @@ connectDB()
 
 //Enable parsing request body
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+// app.use(cors({
+//     origin:'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// }))
 
 //Messages
 app.use('/api/messages', messageRouter)
